98413bbcc3ac19d708e0f3fab148f24d066b393d
Harisegeeks <haris.khan@egeeksglobal.com>
2025-02-21 14:58:51 +0500

    add controller

---
+++ app/Http/Controllers/Auth/logincontroller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function authenticated(Request $request, $user)
+    {
+      $user = Auth::user();
+        if($user->hasRole('admin')){
+            return redirect()->route('admin-dashboard');
+        } else {
+            return redirect()->route('dashboard');
+        }
+    }
+}
